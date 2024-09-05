8add38dede677e4a789e2e153b72d4269d7b6888
madeeha-adam <madeeha@sealmail.net>
2024-09-05 11:58:24 -0700

    Created project for lab 2 with empty main function

---
+++ lab2/lab2/lab2/lab2.cpp
@@ -0,0 +1,19 @@
+/*******************************************************
+ * Name: Madeeha Adam
+ * Assignment: Lab 2 - Making Exact Change
+ * Compiler used: Xcode
+ * Description:
+ *  This program allows the user to check out their own groceries. It asks the user for two inputs; the amount of the purchase and the amount tendered by the customer (assuming the amount tendered is >= the amount of the purchase). The program will then output some lines showing the amount of change returned and the number of bills and coins that will be dispensed as change in descending order of monetary amount.
+ **********************************************************/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ios;
+
+int main()
+{
+    return 0;
+}
+
