fc42255ea140c3d59b35f076d855f99ad25b3741
madeeha-adam <madeeha@sealmail.net>
2024-09-06 12:10:17 -0700

    Added more comments. Lab 2 is complete.

--- lab2/lab2/lab2/lab2.cpp
+++ lab2/lab2/lab2/lab2.cpp
@@ -4,7 +4,7 @@
  * Compiler used: Xcode
  * Description:
  *  This program allows the user to check out their own groceries. It asks the user for two inputs; the amount of the purchase and the amount tendered by the customer (assuming the amount tendered is >= the amount of the purchase). The program will then output some lines showing the amount of change returned and the number of bills and coins that will be dispensed as change in descending order of monetary amount.
- **********************************************************/
+ */
 
 #include <iostream>
 using std::cin;
@@ -14,34 +14,40 @@ using std::ios;
 
 int main()
 {
-    //Create float variable for amount of purchase
+    //Create float variable to store the amount of the purchase
     float purchaseAmount = -1;
     
-    //Create float variable for amount tendered
+    //Create float variable to store the amount tendered
     float amountTendered = -1;
     
+    /* Ask the user for the purchase amount and the amount tendered. If the user enters invalid input, keep asking them the question  until they enter valid input */
     do
     {
-        cout << "Enter the amount of the purchase and the amount tendered (separated by a space): " << endl;
-        cin >> purchaseAmount;
-        cin >> amountTendered;
+        cout << "Enter the amount of the purchase and the amount tendered (separated by a space): " << endl;  //Ask the user for input
+        cin >> purchaseAmount; //get user input and put it in purchaseAmount
+        cin >> amountTendered; //get user input and put it in amountTendered
+        
+        // Check the user's input to see if it is valid or not
         if(cin.fail())
         {
             cin.clear();
             cin.ignore(INT_MAX, '\n');
         }
-    } while(purchaseAmount < 0 || amountTendered < 0);
+    } while(purchaseAmount < 0 || amountTendered < 0); /* Keep executing this loop until the user                                                    enters a valid amount for the two variables*/
     
     //Create a variable to store the amount of change
     float change;
     
     //Determine the amount of change
     change = amountTendered - purchaseAmount;
+    
+    /* Tell the user the amount of change they will receive. Format the output so that the change
+     is shown correct to 2 decimal places */
     cout.setf(ios::fixed);
     cout.precision(2);
     cout << "The change is $" << change <<endl;
     
-    //create a variable for the amount of 50 dollar bills needed for change
+    //create a variable to store how many $50 bills are required for the change
     int fiftyBill = 0;
     
     //Determine if $50 bills should be given as part of the change
@@ -49,11 +55,15 @@ int main()
     {
         while(change > 49.99)
         {
+            //Keep subtracting $50 from the change until the change is less that 50 dollars.
             change = change - 50;
+            
+            //Increment the fiftyBill variable every time a 50 dollar bill is used for change
             fiftyBill++;
             
         }
         
+        //Check to see whether the word "bill" should be plural in the output
         if(fiftyBill > 1)
         {
             cout << fiftyBill << " $50 bills" << endl;
@@ -72,10 +82,14 @@ int main()
     {
         while(change > 19.99)
         {
+            //Keep subtracting $20 from the change until the change is less than 20 dollars.
             change = change - 20;
+            
+            //Increment the twentyBill variable every time a 20 dollar bill is used.
             twentyBill++;
         }
         
+        //Check to see whether the word "bill" should be plural in the output
         if(twentyBill > 1)
         {
             cout << twentyBill << " $20 bills" << endl;
@@ -94,10 +108,14 @@ int main()
     {
         while(change > 9.99)
         {
+            //Keep subtracting $10 from the change until the change is less than 10 dollars.
             change = change - 10;
+            
+            //Increment the tenBill variable every time a 10 dollar bill is used.
             tenBill++;
         }
         
+        //Check to see whether the word "bill" should be plural in the output
         if(tenBill > 1)
         {
             cout << tenBill << " $10 bills" << endl;
@@ -116,10 +134,14 @@ int main()
     {
         while(change > 4.99)
         {
+            //Keep subtracting $5 from the change until the change is less than 5 dollars.
             change = change - 5;
+            
+            //Increment the fiveBill variable every time a 5 dollar bill is used.
             fiveBill++;
         }
         
+        //Check to see whether the word "bill" should be plural in the output
         if(fiveBill > 1)
         {
             cout << fiveBill << " $5 bills" << endl;
@@ -138,10 +160,14 @@ int main()
     {
         while(change > 0.99)
         {
+            //Keep subtracting $1 from the change until the change is less than 1 dollar.
             change = change - 1;
+            
+            //Increment the oneBill variable every time a 1 dollar bill is used.
             oneBill++;
         }
         
+        //Check to see whether the word "bill" should be plural in the output
         if(oneBill > 1 )
         {
             cout << oneBill << " $1 bills" << endl;
@@ -160,10 +186,14 @@ int main()
     {
         while(change > 0.49)
         {
+            //Keep subtracting 50 cents from the change until the change is less than 50 cents
             change = change - 0.50;
+            
+            //Increment the fiftyCoin variable every time a 50-cent coin is used
             fiftyCoin++;
         }
         
+        //Check to see whether the word "coin" should be plural in the output
         if(fiftyCoin > 1)
         {
             cout << fiftyCoin << " 50-cent coins" << endl;
@@ -182,10 +212,14 @@ int main()
     {
         while(change > 0.24)
         {
+            //Keep subtracting 25 cents from the change until the change is less than 25 cents
             change = change - 0.25;
+            
+            //Increment the twentyFiveCoin variable every time a 25-cent coin is used
             twentyFiveCoin++;
         }
         
+        //Check to see whether the word "coin" should be plural in the output
         if(twentyFiveCoin > 1)
         {
             cout << twentyFiveCoin << " 25-cent coins" << endl;
@@ -204,10 +238,14 @@ int main()
     {
         while(change > 0.09)
         {
+            //Keep subtracting 10 cents from the change until the change is less than 10 cents
             change = change - 0.10;
+            
+            //Increment the tenCoin variable every time a 10-cent coin is used
             tenCoin++;
         }
         
+        //Check to see whether the word "coin" should be plural in the output
         if(tenCoin > 1)
         {
             cout << tenCoin << " 10-cent coins" << endl;
@@ -226,10 +264,14 @@ int main()
     {
         while(change > 0.04)
         {
+            //Keep subtracting 5 cents from the change until the change is less than 5 cents
             change = change - 0.05;
+            
+            //Increment the fiveCoin variable every time a 5-cent coin is used
             fiveCoin++;
         }
         
+        //Check to see whether the word "coin" should be plural in the output
         if(fiveCoin > 1)
         {
             cout << fiveCoin << " 5-cent coins" << endl;
@@ -248,10 +290,14 @@ int main()
     {
         while(change > 0.009)
         {
+            //Keep subtracting 1 cent from the change until the change is less than 1 cent
             change = change - 0.01;
+            
+            //Increment the oneCoin variable every time a 1-cent coin is used
             oneCoin++;
         }
         
+        //Check to see whether the word "coin" should be plural in the output
         if(oneCoin > 1)
         {
             cout << oneCoin << " 1-cent coins" << endl;
