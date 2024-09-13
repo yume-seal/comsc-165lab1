e0935c0d13feb9abf7a51c8ab59d24b7cbaff3b2
madeeha-adam <madeeha@sealmail.net>
2024-09-13 13:18:11 -0700

    Added code to main function to print welcome message

---
+++ lab2a/lab2a/main.cpp
@@ -0,0 +1,20 @@
+/*
+*  Name: Madeeha Adam
+*  Assignment: Lab 2A: Making Exact Change Using Functions
+* Compiler used: Xcode
+*  Description:
+*   This program allows the user to check out their own groceries. It asks the user for two inputs; the amount of the purchase and the amount tendered by the customer (assuming the amount tendered is >= the amount of the purchase). The program will then output some lines showing the amount of change returned and the number of bills and coins that will be dispensed as change in descending order of monetary amount.
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ios;
+
+int main(int argc, const char * argv[]) {
+    
+    // Print Welcome message
+    cout << "Welcome to My Change Program" << endl;
+    return 0;
+}
