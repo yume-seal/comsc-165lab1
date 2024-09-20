2a114bcb2b38bc089d0a654eedf978abae1176a5
madeeha-adam <madeeha@sealmail.net>
2024-09-20 16:31:26 -0700

    Played with more things... none of the table information is properly centred

--- examplething/example/example/main.cpp
+++ examplething/example/example/main.cpp
@@ -13,6 +13,18 @@ int main(int argc, const char * argv[]) {
  //   std::cout << "Hello World!\n";
    // std::cout << std::setw(20) << std::left << "Hello, World!"
      //   << std::setw(30) << std::left << "Name\n";
-    std::cout << std::setw(0) << std::setfill('-') << std::setw(50) << "" << std::endl << std::setfill(' ');
+    
+    std::cout << std::left << "Name";
+    std::cout << std::setw(10) << std::right << "Age";
+    std::cout << std::setw(10) << "City";
+    std::cout << std::setw(20) << "Class Rank";
+    std::cout << std::setw(7) << "GPA\n";
+    std::cout << std::setw(0) << std::setfill('-') << std::setw(51) << "" << std::endl << std::setfill(' ');
+    std::cout << std::left << "Some Name";
+    std::cout << std::setw(10) << std::right << "31";
+    std::cout << std::setw(10) << "Some City";
+    std::cout << std::setw(20) << "32";
+    std::cout << std::setw(7) << "2.0\n";
+    std::cout << std::setw(0) << std::setfill('-') << std::setw(51) << "" << std::endl << std::setfill(' ');
     return 0;
 }
