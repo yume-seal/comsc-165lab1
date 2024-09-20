a85a5acb00fc3158fcb03d08573fb84b4ab976a2
madeeha-adam <madeeha@sealmail.net>
2024-09-20 15:06:21 -0700

    updated name of lab2a and started a new project to practice something

---
+++ examplething/example/example/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  example
+//
+//  Created by Sufyan Adam on 20/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
