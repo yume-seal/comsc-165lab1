7eeef1cae5f6eea2b2f3a58c1c8dca7bd859a010
madeeha-adam <madeeha@sealmail.net>
2024-09-29 13:10:54 -0700

    First commit- project started

---
+++ lab4assignment/lab4assignment/lab4.cpp
@@ -0,0 +1,26 @@
+/*
+*  main.cpp
+*  lab4assignment
+*
+*  Created by Sufyan Adam on 29/9/24.
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ios;
+
+#include <iomanip> //so we can use setw and stuff
+
+#include <string>
+using std::string;
+
+#include <fstream> //so we can do serialisation
+using std::ofstream;
+using std::ifstream;
+
+int main()
+{
+    return 0;
+}
