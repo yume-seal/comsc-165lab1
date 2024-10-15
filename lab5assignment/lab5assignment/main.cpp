dc5f553b1615b0f1a2d9e5ad44a272ae8a6b9460
madeeha-adam <madeeha@sealmail.net>
2024-10-15 12:28:49 -0700

    Created the spend function

--- lab5assignment/lab5assignment/main.cpp
+++ lab5assignment/lab5assignment/main.cpp
@@ -22,6 +22,40 @@ struct Expense {
     int amount;
 };
 
+Expense spend()
+{
+    Expense newExpense;
+    cout << "Please enter the description for the expense: ";
+    string desc;
+    cin.ignore();
+    getline(cin, desc);
+    
+    while(desc.empty())
+    {
+        cout << "\nThe description cannot be empty. Please try again: ";
+        cin.clear();
+        getline(cin, desc);
+    }
+    
+    newExpense.description = desc;
+    
+    cout << "Please enter the amount of the expense: ";
+    float amount;
+    cin >> amount;
+    
+    while(amount == 0)
+    {
+        cout << "\nThe amount cannot be 0. Please try again: ";
+        cin.clear();
+        cin.ignore(INT_MAX, 10);
+        cin >> amount;
+    }
+    
+    newExpense.amount = amount;
+    
+    return newExpense;
+}
+
 void showAll(Expense expenses[], int size)
 {
     for(int counter = 0; counter <= size; counter++)
@@ -37,34 +71,43 @@ void showAll(Expense expenses[], int size)
     }
 }
 
-void menu(Expense expenses[], int size)
+void menu(Expense expenses[], int& size)
 {
-    cout << "Expense Tracking Menu" << endl;
-    cout << std::right << std::setw(13) << "1. Show all" << endl;
-    cout << std::right << std::setw(10) << "2. Spend" << endl;
-    cout << std::right << std::setw(43) << "3. Search expenses containing this string" << endl;
-    cout << std::right << std::setw(62) << "4. Search expenses with greater than or equal to this amount" << endl;
-    cout << std::right << std::setw(9) << "5. Exit" << endl;
-    
-    cout << "Enter your option: ";
-    
     int option = 0;
     string buff;
     
-    cin >> buff;
-    option = atoi(buff.c_str());
-    
-    while(option < 1 || option > 5)
+    while(option != 5)
     {
-        cout << "\nInvalid input. Please enter an integer between 1 and 5: ";
-        cin.ignore(INT_MAX, '\n');
-        cin.clear();
-        cin >> option;
-    }
-    
-    if(option == 1)
-    {
-        showAll(expenses, size);
+        cout << "Expense Tracking Menu" << endl;
+        cout << std::right << std::setw(13) << "1. Show all" << endl;
+        cout << std::right << std::setw(10) << "2. Spend" << endl;
+        cout << std::right << std::setw(43) << "3. Search expenses containing this string" << endl;
+        cout << std::right << std::setw(62) << "4. Search expenses with greater than or equal to this amount" << endl;
+        cout << std::right << std::setw(9) << "5. Exit" << endl;
+        
+        cout << "Enter your option: ";
+        
+        cin >> buff;
+        option = atoi(buff.c_str());
+        
+        while(option < 1 || option > 5)
+        {
+            cout << "\nInvalid input. Please enter an integer between 1 and 5: ";
+            cin.ignore(INT_MAX, '\n');
+            cin.clear();
+            cin >> option;
+        }
+        
+        if(option == 1)
+        {
+            showAll(expenses, size);
+        }
+        else if(option == 2)
+        {
+            size++;
+            Expense newExpense = spend();
+            expenses[size] = newExpense;
+        }
     }
 }
 
@@ -72,7 +115,7 @@ int main()
 {
     int const CAPACITY = 100;
     Expense expenses[CAPACITY];
-    int size = 0;
+    int size = -1;
     menu(expenses, size);
     return 0;
 }
