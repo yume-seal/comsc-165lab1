ee640c8795ab772d7fd2907bf77180ae81e7c93f
madeeha-adam <madeeha@sealmail.net>
2024-11-07 19:55:27 -0800

    Created the menu function

--- lab6assignment/lab6assignment/main.cpp
+++ lab6assignment/lab6assignment/main.cpp
@@ -23,7 +23,58 @@ struct Movie
     int rating;
     Movie* next;
 };
+
+void menu()
+{
+    char choice;
+    
+    do
+    {
+        cout << "MENU\n";
+        cout << "A Add a movie\n";
+        cout << "U Update a movie\n";
+        cout << "E rEmove a movie\n";
+        cout << "L List all movies\n";
+        cout << "T arrange by Title\n";
+        cout << "V arrange by year Viewed\n";
+        cout << "R arrange by Rating\n";
+        cout << "Q Quit\n";
+        cout << "...your choice: ";
+        cin >> choice;
+        
+        if(choice == 'A' || choice == 'a')
+        {
+            //call some function to add a movie
+        }
+        else if(choice == 'U' || choice == 'u')
+        {
+            //call some function to update movie
+        }
+        else if(choice == 'E' || choice == 'e')
+        {
+            //call dome function to remove a movie
+        }
+        else if(choice == 'L' || choice == 'l')
+        {
+            //call some function to list all movies
+        }
+        else if(choice == 'T' || choice == 't')
+        {
+            //call some function to arrange by title
+        }
+        else if(choice == 'V' || choice == 'v')
+        {
+            //call some function to arrange by year viewed
+        }
+        else if(choice == 'R' || choice == 'r')
+        {
+            //call some function to arrange by rating
+        }
+    } while (choice != 'Q' && choice != 'q');
+}
 int main()
 {
+    Movie* head = nullptr;
+    menu();
     return 0;
 }
