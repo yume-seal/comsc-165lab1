df6c293941e980f57a81c455cdccb82cc3991cc8
madeeha-adam <madeeha@sealmail.net>
2024-11-15 10:15:17 -0800

    Created the add movie function

--- lab7assignment/lab7assignment/main.cpp
+++ lab7assignment/lab7assignment/main.cpp
@@ -64,6 +64,56 @@ void serialiseUp(Movie*& firstMoviePtr, Movie*& lastMoviePtr)
     fin.close();
 }
 
+/* This function allows the user to create a new movie object and put it in the linked list
+ Params: Movie pointer
+ Return: NONE*/
+void addMovie(Movie*& firstMoviePtr, Movie*& lastMoviePtr)
+{
+    string title; //string variable for title of the movie
+    cout << "\nEnter the movie's name: "; //ask user for input
+    cin.ignore(); //ignore whitespace...
+    getline(cin, title); //get user input
+    Movie* temp = new Movie; //create a temporary movie pointer and put a new movie object in it
+    temp->title = title; //set the user entered title as the title of the movie
+    int year; //integer variable for the year the movie was viewed.
+    string buff;
+    cout << "\nEnter the year you saw " << title << " [like 2016]: ";
+    cin >> buff;
+    year = atoi(buff.c_str());
+    while(year < 1888) //make sure this is a valid time to have seen a movie
+    {
+        cout << "\nInvalid input. Enter the year you saw " << title << " [like 2016]: ";
+        cin.clear();
+        cin.ignore();
+        cin >> buff;
+        year = atoi(buff.c_str());
+    }
+    temp->year = year;
+    int rating;
+    cout << "\nEnter your rating for " << title << " [1, 2, 3, 4, 5]: ";
+    cin >> buff;
+    rating = atoi(buff.c_str());
+    while(rating < 1 || rating > 5) //make sure the rating is in the interval
+    {
+        cout << "\nInvalid rating. Enter your rating for " << title << " [1, 2, 3, 4, 5]: ";
+        cin.clear();
+        cin.ignore();
+        cin >> buff;
+        rating = atoi(buff.c_str());
+    }
+    temp->rating = rating;
+    if(firstMoviePtr == nullptr)
+    {
+        firstMoviePtr = temp;
+        lastMoviePtr = temp;
+    }
+    else
+    {
+        lastMoviePtr->next = temp;
+        lastMoviePtr = temp;
+    }
+}
+
 /* This function prints out the menu
  Params: Movie pointer, integer value
  Return: NONE*/
@@ -88,7 +138,7 @@ void menu(Movie*& firstMoviePtr, Movie*& lastMoviePtr, int& sequenceNumber)
         //evaluate user's choice and call appropriate function
         if(choice == 'A' || choice == 'a')
         {
-            //addMovie(head);
+            addMovie(firstMoviePtr, lastMoviePtr);
             sequenceNumber++;
         }
         else if(choice == 'U' || choice == 'u')
