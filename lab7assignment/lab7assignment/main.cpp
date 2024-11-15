a651cb16d1945291f08377b82bae8cf76b9ce2bd
madeeha-adam <madeeha@sealmail.net>
2024-11-15 09:39:14 -0800

    Created movie struct

---
+++ lab7assignment/lab7assignment/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  lab7assignment
+//
+//  Created by Sufyan Adam on 14/11/24.
+//
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::ios;
+using std::endl;
+
+#include <fstream>
+using std::ifstream;
+using std::ofstream;
+
+#include <iomanip>
+
+#include <algorithm>
+using std::swap;
+
+#include <string>
+using std::string;
+
+struct Movie //create movie struct
+{
+    string title;
+    int year;
+    int rating;
+    Movie* next;
+};
+
+int main() {
+    return 0;
+}
