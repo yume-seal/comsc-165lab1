e5c0123f1fe905c584caf3485d8361c52fd15cea
madeeha-adam <madeeha@sealmail.net>
2024-12-14 13:26:44 -0800

    created expense class in expense.h

---
+++ lab10assignment/lab10assignment/expense.h
@@ -0,0 +1,30 @@
+//
+//  expense.h
+//  lab10assignment
+//
+//  Created by Sufyan Adam on 14/12/24.
+//
+
+#ifndef EXPENSE_H
+#define EXPENSE_H
+#include <string>
+using std::string;
+
+class Expense
+{
+private:
+    double amount;
+    string description;
+    
+public:
+    Expense();
+    Expense(double amount, const string description);
+    double getAmount() const;
+    void setAmount(double amount);
+    string& getDescription() const;
+    
+    void setDescription(string& description);
+    
+    bool contains(string& searchString) const;
+};
+#endif
